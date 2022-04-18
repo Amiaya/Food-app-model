2c707dd4bf59d09132a0365973ad4584f677538c
Amiaya <amiayaprime@gmail.com>
2022-04-18 18:50:47 +0100

    add production setup

--- Controller/errorController.js
+++ Controller/errorController.js
@@ -16,18 +16,47 @@ const handleValidationErrorDB = err => {
     return new AppError(message,400)
 }
 
-module.exports = (err, req,res,next) => {
-    err.statusCode = err.statusCode || 500
-    err.status = err.status || 'error'
 
-    if(err.code === 11000) err = handleDuplicatedFieldDB(err)
-    if(err.name === "CastError") err = handleCastErrorDB(err)
-    if(err.name === "ValidationError") err = handleValidationErrorDB(err)
 
+const sendErrorProd = (err,res) => {
+    if(err.isOperational){
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message,
+            
+        })
+    }else{
+        console.error('Error', err)
+        res.status(500).json({
+            status: "error",
+            message: "Something went wrong"
+        })
+    }
+    
+}
+
+const sendErrorDev = (err,res) => {
     res.status(err.statusCode).json({
         status: err.status,
         error: err,
         message: err.message,
         stack: err.stack
     })
+}
+
+module.exports = (err, req,res,next) => {
+    err.statusCode = err.statusCode || 500
+    err.status = err.status || 'error'
+
+    if(process.env.NODE_ENV === 'development'){
+        sendErrorDev(err,res)
+    }
+
+    else if(process.env.NODE_ENV === 'production'){
+        if(err.code === 11000) err = handleDuplicatedFieldDB(err)
+        if(err.name === "CastError") err = handleCastErrorDB(err)
+        if(err.name === "ValidationError") err = handleValidationErrorDB(err)
+        sendErrorProd(err,res)
+    }
+    
 }
